ac37aa0e56c45eb751276e41d2e467f3c3e17ba6
jorge asillo <jorge.asillo@gmail.com>
2024-05-21 15:10:18 -0500

    Game Item Model
    + scriptable object of items
    + hats items

---
+++ Assets/_Scripts/Scriptable Objects/SOGameItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(fileName = "item", menuName = "MyGame/item", order = 1)]
+    public class SOGameItem : ScriptableObject
+    {
+
+        [SerializeField] TGameItem _type;
+        [SerializeField] private string _name;
+        [SerializeField] private Sprite[] _icons;
+        [SerializeField] private int _price;
+        [SerializeField] private int _code;
+
+        public TGameItem Type => _type;
+        public string Name => _name;
+        public Sprite[] Icons => _icons;
+        public int Price => _price;
+
+        public int Code => _code;
+    }
+
+    
+}
